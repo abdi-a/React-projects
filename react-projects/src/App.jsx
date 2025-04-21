4700cf2cb89da24424bee3a880bce2744a54691a
abdisa awel <abdisaawel313@gmail.com>
2025-04-21 21:14:48 +0300

    lastly

--- react-projects/src/App.jsx
+++ react-projects/src/App.jsx
@@ -1,18 +1,18 @@
-// import RollDice from './components/roll-dice/rollDice'
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { fas } from '@fortawesome/free-solid-svg-icons'
-// library.add(fas)
-// import PasswordValidator from "./components/password-validator/index"
-// import Joke from "./components/joke-generators/joke";
-// import SubmissionForm from "./components/submission-form/index"
-// import ToDo from "./components/to-do-app/App"
-// import Dictionary from "./components/Dictionary/Dictionary"
-// import QrCodeGenerator from "./components/Qr-code-generator/App"
-// import WordCount from "./components/word-count/App"
-// import Memes from "./components/memes/App"
-// import WeatherApp from "./components/weather-app/App"
-// import TaskSchedule from "./components/task-scheduler/App"
-// import LanguageTranslator from "./components/Language-translator/App"
+import RollDice from './components/roll-dice/rollDice'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+import PasswordValidator from "./components/password-validator/index"
+import Joke from "./components/joke-generators/joke";
+import SubmissionForm from "./components/submission-form/index"
+import ToDo from "./components/to-do-app/App"
+import Dictionary from "./components/Dictionary/Dictionary"
+import QrCodeGenerator from "./components/Qr-code-generator/App"
+import WordCount from "./components/word-count/App"
+import Memes from "./components/memes/App"
+import WeatherApp from "./components/weather-app/App"
+import TaskSchedule from "./components/task-scheduler/App"
+import LanguageTranslator from "./components/Language-translator/App"
 
 
 
@@ -23,21 +23,21 @@ function App() {
 
   return (
     <>
-     {/* <div className="App">
+     <div className="App">
             <h1>Joke Generator Using React and Joke API</h1>
             <Joke/>
-        </div> */}
-{/* <SubmissionForm /> */}
-{/* <PasswordValidator/>
+        </div>
+<SubmissionForm /> 
+<PasswordValidator/>
  <RollDice />
  <ToDo/>
  <QrCodeGenerator/>
  <Dictionary />
- <Memes/> */}
- {/* <WordCount/> */}
- {/* <WeatherApp/> */}
- {/* <TaskSchedule/> */}
- {/* <LanguageTranslator/> */}
+ <Memes/>
+  <WordCount/>
+ <WeatherApp/> 
+ <TaskSchedule/>
+ <LanguageTranslator/>
     </>
   )
 }
