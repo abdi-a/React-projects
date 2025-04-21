0e6489b9f95e2e00bfefd11f791be5f155f654de
abdisa awel <abdisaawel313@gmail.com>
2025-04-21 16:11:37 +0300

    word Count

---
+++ react-projects/src/components/word-count/App.jsx
@@ -0,0 +1,16 @@
+// App.js
+import React from "react";
+import WordLetterCounter from "./wordLetterCount";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 id="top">GeeksforGeeks</h1>
+      <h1>Words and Letters Counter</h1>
+      <WordLetterCounter />
+    </div>
+  );
+}
+
+export default App;
